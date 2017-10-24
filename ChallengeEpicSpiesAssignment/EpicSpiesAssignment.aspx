2d76184d0347b0c56dacbf8783ea2fa934c1bb0b
Jeff Ferrell <jwferrell78@gmail.com>
2017-10-23 17:18:08 -0700

    Calendars and Conditionals
    Working with DateTime and TimeSpan objects in the Visual Studio / C#, to create a scheduling website based on the business/union requirements of minium rest and bonus pay.

---
+++ ChallengeEpicSpiesAssignment/EpicSpiesAssignment.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="EpicSpiesAssignment.aspx.cs" Inherits="ChallengeEpicSpiesAssignment.EpicSpiesAssignment" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            font-weight: normal;
+        }
+        .auto-style3 {
+            font-weight: normal;
+            font-size: large;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Image ID="Image1" runat="server" Height="190px" ImageUrl="~/epic-spies-logo.jpg" Width="160px" />
+        <h1><strong style="font-family: Arial, Helvetica, sans-serif">Spy New Assignment Form</strong></h1>
+        <h3><span class="auto-style1">Spy Code Name:</span>&nbsp;
+            <asp:TextBox ID="codeNameTextBox" runat="server" Width="189px"></asp:TextBox>
+        </h3>
+        <h3><span class="auto-style3">New Assignment Name:</span>&nbsp;
+            <asp:TextBox ID="assignmentNameTextBox" runat="server" Width="188px"></asp:TextBox>
+        </h3>
+        <p>
+            &nbsp;</p>
+        <p class="auto-style3">
+            End Date of Previous Assignment:<asp:Calendar ID="previousEndCalendar" runat="server"></asp:Calendar>
+        </p>
+        <p class="auto-style3">
+            &nbsp;</p>
+        <p class="auto-style3">
+            Start Date of New Assignment:<asp:Calendar ID="newCalendar" runat="server"></asp:Calendar>
+        </p>
+        <p class="auto-style3">
+            &nbsp;</p>
+        <p class="auto-style3">
+            Projected End Date of New Assignment:<asp:Calendar ID="newEndCalendar" runat="server"></asp:Calendar>
+            &nbsp;</p>
+        <p class="auto-style3">
+            <asp:Button ID="assignButton" runat="server" OnClick="assignButton_Click" Text="Assign Spy" />
+        </p>
+        <p class="auto-style3">
+            <asp:Label ID="detailsLabel" runat="server"></asp:Label>
+        </p>
+        <p class="auto-style3">
+            &nbsp;</p>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ ChallengeEpicSpiesAssignment/EpicSpiesAssignment.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeEpicSpiesAssignment
+{
+    public partial class EpicSpiesAssignment : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            MaintainScrollPositionOnPostBack = true;
+
+            if (!Page.IsPostBack)
+            {
+                previousEndCalendar.SelectedDate = DateTime.Today;
+                newCalendar.SelectedDate = DateTime.Today.AddDays(14);
+                newCalendar.VisibleDate = DateTime.Today.AddDays(14);
+                newEndCalendar.SelectedDate = DateTime.Today.AddDays(21);
+                newEndCalendar.VisibleDate = DateTime.Today.AddDays(21);
+            }       
+        }
+
+        protected void assignButton_Click(object sender, EventArgs e)
+        {
+
+            TimeSpan newAssignmentDuration = newEndCalendar.SelectedDate.Subtract(newCalendar.SelectedDate);
+            TimeSpan restBetweenDuration = newCalendar.SelectedDate.Subtract(previousEndCalendar.SelectedDate);
+
+            detailsLabel.Text = String.Format("Assignment of {0} to assignment Project: {1} is authorized. Budget Total: {2:C}",
+            codeNameTextBox.Text, assignmentNameTextBox.Text, newAssignmentDuration.Days * 500);
+
+            if (restBetweenDuration.Days < 14)
+                detailsLabel.Text = "Error:  Must allow at least two weeks between previous assignment and new assignment.";
+                newCalendar.SelectedDate = previousEndCalendar.SelectedDate.AddDays(14);
+                newCalendar.VisibleDate = previousEndCalendar.SelectedDate.AddDays(14);
+
+            if (codeNameTextBox.Text.Length < 1 && assignmentNameTextBox.Text.Length < 1
+                || codeNameTextBox.Text.Length < 1
+                || assignmentNameTextBox.Text.Length < 1)
+                detailsLabel.Text = "Please make sure that you have entered a Code Name and Assignment Name.";
+
+            if (newAssignmentDuration.Days > 21)
+                detailsLabel.Text = String.Format("Assignment of {0} to assignment Project: {1} is authorized. Budget Total: {2:C}",
+                codeNameTextBox.Text, assignmentNameTextBox.Text, (newAssignmentDuration.Days * 500) + 1000);
+
+        }
+    }
+}
